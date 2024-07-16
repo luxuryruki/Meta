1fa9322218bb821eae3e2d77a5fc6556e4814e5b
KANG JIHYO <luxuryjihyo@naver.com>
2024-07-16 18:53:03 +0900

    페이스북 로그인 페이지

--- src/main/java/com/example/meta/controller/AuthController.java
+++ src/main/java/com/example/meta/controller/AuthController.java
@@ -2,6 +2,10 @@ package com.example.meta.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
 
 @Controller
 public class AuthController {
@@ -9,6 +13,13 @@ public class AuthController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @PostMapping("/login")
+    public void login(@RequestBody Map<String, Object> payload) {
+        // 여기서 사용자 정보를 처리합니다.
+        System.out.println("Received user data: " + payload);
+
     }
     
 }
